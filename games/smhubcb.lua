4c0f477315e8b1ad9add38bbdbe7e2966a7f0fe2
stam <95550655+gerstam@users.noreply.github.com>
2026-02-11 21:41:42 +0100

    Update loadstrings to self-hosted versions

---
+++ games/smhubcb.lua
@@ -0,0 +1,3 @@
+-- Load external scripts using self-hosted versions
+loadstring(game:HttpGet('https://raw.githubusercontent.com/gerstam/CB-Script/main/path/to/script1.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/gerstam/CB-Script/main/path/to/script2.lua'))()
